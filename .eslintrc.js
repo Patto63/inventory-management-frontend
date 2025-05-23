ae4d2e6eaad3d8e4541e0c5085321b274f0ab959
Fernandofr7 <lsanchez6604@uta.edu.ec>
2025-05-23 17:43:47 -0500

    Editar el workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
-  extends: [
-    'next',
-    'next/core-web-vitals',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  plugins: ['react', '@typescript-eslint'],
+  extends: ['next', 'next/core-web-vitals'],
   rules: {
-    'react/react-in-jsx-scope': 'off', // No es necesario en Next.js 12+
-    'react/prop-types': 'off',         // Innecesario si usas TypeScript
-    '@typescript-eslint/no-empty-interface': 'off', // Para permitir interfaces vacías
-  },
-  settings: {
-    react: {
-      version: 'detect', // Detectar automáticamente la versión de React
-    },
+    'react/react-in-jsx-scope': 'off', // no se necesita en Next.js 12+
+    'react/prop-types': 'off', // si usas TypeScript
   },
 };
